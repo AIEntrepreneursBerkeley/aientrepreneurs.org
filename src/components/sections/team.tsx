346c4f80050e862c1dd5ebab6063bf9bb7c9e890
Anthony Kuang <anthonykuang@berkeley.edu>
2025-08-30 09:11:35 -0700

    test in prod

--- src/components/sections/team.tsx
+++ src/components/sections/team.tsx
@@ -7,49 +7,55 @@ const Team = () => (
         name={"Jyoti Rani"}
         image={"/images/jyoti.jpg"}
         position={"President"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/jyotiinar/"}
       />
       <Card
         name={"Vardaan Tekriwal"}
         image={"/images/vardaan.jpg"}
         position={"Exec"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/vtekriwal2211/"}
       />
       <Card
         name={"Stephen Xie"}
         image={"/images/stephen.jpg"}
         position={"Exec"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/stephen-xie/"}
       />
       <Card
         name={"Alex Mehregan"}
         image={"/images/alex.jpg"}
         position={"Exec"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/alex-mehregan/"}
       />
       <Card
         name={"Dylan Westland"}
         image={"/images/dylan.jpg"}
         position={"Exec"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/dylanwestland/"}
       />
       <Card
         name={"Ari Ramsan"}
         image={"/images/ari.jpg"}
         position={"Exec"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/ari-ramsan/"}
       />
       <Card
         name={"Rohan Sontakke"}
         image={"/images/rohan.jpg"}
         position={"Exec"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/rohan-sontakke-rss/"}
       />
       <Card
-        name={"Rohan Sontakke"}
-        image={"/images/rohan.jpg"}
+        name={"Diya Girish Kumar"}
+        image={"/images/diya.jpg"}
         position={"Exec"}
-        linkedin={"https://www.linkedin.com"}
+        linkedin={"https://www.linkedin.com/in/diyagirishkumar/"}
+      />
+      <Card
+        name={"Rushil Mohan"}
+        image={"/images/rushil.jpg"}
+        position={"Exec"}
+        linkedin={"https://www.linkedin.com/in/rushilmoh/"}
       />
     </div>
     <div className="xs:grid-cols-2 container mt-32 grid grid-cols-1 gap-x-3.5 gap-y-6 px-4 md:grid-cols-3 lg:grid-cols-4"></div>
