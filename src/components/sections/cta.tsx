91a2d487b2bb40fd7c8dcdf32bf4d4d7bdef9467
Anthony Kuang <anthonykuang@berkeley.edu>
2025-08-30 08:48:08 -0700

    add link

--- src/components/sections/cta.tsx
+++ src/components/sections/cta.tsx
@@ -4,7 +4,7 @@ import { Prism } from "~/components/ui/prism";
 export const CTA = () => (
   <section className="bg-background relative -top-24 pt-72">
     <div className="relative container h-[1000px] px-0">
-      <div className="relative">
+      <div className="relative h-full w-full">
         <Prism
           animationType="hover"
           timeScale={0.5}
@@ -19,11 +19,16 @@ export const CTA = () => (
           glow={1.5}
         />
         <div className="absolute inset-0 flex items-center justify-center">
-          <CircularText
-            text="AI✦Entrepreneurs✦at✦Berkeley✦"
-            onHover="goBonkers"
-            className="place-content-center"
-          />
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeczWIYO9PKcUJVRSHdIt4z6mt2XkQ2lgcBySjiA5IdLnxoWA/viewformg"
+            target="_blank"
+          >
+            <CircularText
+              text="AI✦Entrepreneurs✦at✦Berkeley✦"
+              onHover="goBonkers"
+              className="place-content-center"
+            />
+          </a>
         </div>
       </div>
     </div>
